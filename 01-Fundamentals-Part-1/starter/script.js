e01a2f747dba2e68791e974f2c435dbc2c217bec
Vinicius Farias <fsvinicius9@gmail.com>
2022-09-13 14:05:05 -0300

    Alteracoes fundamentais

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,53 @@
-let js = 'amazing';
+let js = "amazing";
 
-if(js === 'amazing') alert('Javascript is amazing')
-      40+8+23-10;
+if (js === "amazing") alert("Javascript is amazing");
+40 + 8 + 23 - 10;
+
+// console.log("Jonas");
+
+// let firstName = "Jonas";
+// let jonas_matilda = "JM";
+
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
+// console.log(typeof true)
+// console.log(typeof 'Jonas')
+// console.log(typeof 23)
+// console.log(typeof null)
+job = "teacher";
+var job = "programmer";
+let age = 30;
+lastName = "Vinicius Farias";
+console.log(lastName);
+
+const birthYear = 1991;
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas - ageSarah);
+console.log(ageJonas * 3);
+//2 ** 3 means 2 to the power of 3
+
+//Concatenation or use template strings
+const firstName = "Vinicius";
+const lastName = "Farias";
+console.log(firstName + " " + lastName);
+
+//Assignment operators
+let x = 10 + 5;
+X += 20;
+x *= 4;
+
+//Comparison operators
+
+console.log(ageJonas > ageSarah);
+console.log(ageSah >= 18);
+const isFullAge = ageSarah >= 18;
+// > ->greater
+//x = x+ 10;
+//reassigning the x value
+//times = vezes(asteristicos)
+//asterisk
+//var deve ser evitada, utiliza em codigos antigos
+
+console.log(now - 19991 > now - 2018);
