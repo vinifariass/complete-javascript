4e144804d6a703e4eadb17bcadf3b66c95eb90da
Vinicius Farias <fsvinicius9@gmail.com>
2022-09-14 09:48:58 -0300

    Coding Challenge 3

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -225,3 +225,54 @@ if (hasDriversLicense & hasGoodVision && !isTired) {
 }
 
 console.log(isTired || hasGoodVision || hasDriversLicense);
+
+///////////////////////////////////
+// Coding Challenge #3
+
+/*
+There are two gymnastics teams, Dolphins and Koalas. They compete against each other 3 times. The winner with the highest average score wins the a trophy!
+
+1. Calculate the average score for each team, using the test data below
+2. Compare the team's average scores to determine the winner of the competition, and print it to the console. Don't forget that there can be a draw, so test for that as well (draw means they have the same average score).
+
+3. BONUS 1: Include a requirement for a minimum score of 100. With this rule, a team only wins if it has a higher score than the other team, and the same time a score of at least 100 points. HINT: Use a logical operator to test for minimum score, as well as multiple else-if blocks 😉
+4. BONUS 2: Minimum score also applies to a draw! So a draw only happens when both teams have the same score and both have a score greater or equal 100 points. Otherwise, no team wins the trophy.
+
+TEST DATA: Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110
+TEST DATA BONUS 1: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123
+TEST DATA BONUS 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
+
+GOOD LUCK 😀
+*/
+var dolphins = (96 + 108 + 89) / 3;
+var koalas = 97 + 112 + 101 / 3;
+var averageTeams = (dolphins + koalas) / 2;
+if (dolphins > koalas) {
+  console.log("Dolphins wins");
+} else if (koalas > dolphins) {
+  console.log("Koalas wins");
+} else if (dolphins == koalas) {
+  console.log("DRAW!");
+}
+
+var dolphins = (97 + 112 + 101) / 3;
+var koalas = 109 + 95 + 123 / 3;
+
+if (dolphins > koalas && dolphins >= 100) {
+  console.log("Dolphins wins with a score higher than 100");
+} else if (koalas > dolphins && koalas >= 100) {
+  console.log("Koalas wins with a score higher than 100");
+} else if (dolphins == koalas) {
+  console.log("DRAW!");
+}
+
+var dolphins = (97 + 112 + 101) / 3;
+var koalas = (109 + 95 + 106) / 3;
+
+if (dolphins > koalas && dolphins >= 100) {
+  console.log("Dolphins wins with a score higher than 100");
+} else if (koalas > dolphins && koalas >= 100) {
+  console.log("Koalas wins with a score higher than 100");
+} else if (dolphins == koalas && dolphins >= 100 && koalas >= 100) {
+  console.log("DRAW!");
+}
