dc0fe1176831f777a1d26ae88b919875ce52e255
Vinicius Farias <fsvinicius9@gmail.com>
2022-09-14 21:11:47 -0300

    fruitProcessor function

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,24 @@
+"use strict";
+function logger() {
+  console.log("My name is jonas");
+}
+
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+  console.log();
+
+  const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+
+  return juice;
+}
+
+fruitProcessor(5, 0);
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+console.log(fruitProcessor(5, 0));
+
+const appleOrangeJuice = fruitProcessor(2, 4);
