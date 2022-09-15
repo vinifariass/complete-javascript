44b2d3c0697bb95f85d0e37cffc084379258c447
Vinicius Farias <fsvinicius9@gmail.com>
2022-09-15 07:57:06 -0300

    arrow function calcage3

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -44,4 +44,17 @@ const age2 = calcAge2(1991);
 console.log(age1, age2);
 
 //Arrow function is a shorter form but faster to write
+// are excellent for function of one line
 const calcAge3 = (birthYear) => 2037 - birthYear;
+
+const age3 = calcAge1(1991);
+
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+  const age = 2037 - birthYear;
+  const retirement = 65 - age;
+  return `${firstName} retires in ${retirement} years`;
+};
+
+console.log(yearsUntilRetirement(1991, "Vinicius"));
