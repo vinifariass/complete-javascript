71c9f5000054935c51395fd06903cb68e88738ac
Vinicius <fsvinicius9@gmail.com>
2022-09-20 11:19:51 -0300

    calcTip

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -139,6 +139,61 @@ const age2 = calcAge(years[1]);
 //Basic Operators Methods
 
 // method push -> add an element in the final of n array
+//Add elements
+const friendsS = ["Michael", "Steven", "Peter"];
+const newLength = friendsS.push("Jay");
+console.log(newLength);
+friends.unshift("John");
+//Remove elements
+const popped = friends.pop();
+console.log(popped);
+console.log("John");
+friends.unshift(); //First
+//search a parameter in
+console.log(friends.indexOf("Steven"));
 
-const friendsS = ['Michael'.'Steven','Peter'];
-friendsS.push('Jay')
+//verifica se esta dentro do array
+console.log(friends.includes("Steven"));
+
+if (friends.includes("Peter")) {
+  console.log("You have a friend called Peter");
+}
+
+///////////////////////////////////////
+// Coding Challenge #2
+
+/*
+Steven is still building his tip calculator, using the same rules as before: Tip 15% of the bill if the bill value is between 50 and 300, and if the value is different, the tip is 20%.
+
+1. Write a function 'calcTip' that takes any bill value as an input and returns the corresponding tip, calculated based on the rules above (you can check out the code from first tip calculator challenge if you need to). Use the function type you like the most. Test the function using a bill value of 100.
+2. And now let's use arrays! So create an array 'bills' containing the test data below.
+3. Create an array 'tips' containing the tip value for each bill, calculated from the function you created before.
+4. BONUS: Create an array 'total' containing the total values, so the bill + tip.
+
+TEST DATA: 125, 555 and 44
+
+HINT: Remember that an array needs a value in each position, and that value can actually be the returned value of a function! So you can just call a function as array values (so don't store the tip values in separate variables first, but right in the new array) 😉
+
+GOOD LUCK 😀
+*/
+
+var bill = 275;
+
+console.log(
+  `The bill was ${bill}, the tip was ${tip}, and the total value ${total}`
+);
+
+function calcTip(bill) {
+  var tip = bill > 50 && bill < 300 ? 0.15 : 0.2;
+  var total = bill * tip;
+  return total;
+}
+let bill1 = 125
+let bill2 = 555
+let bill3 = 44
+let bills = [bill1,bill2,bill3]
+
+let tips1 =calcTip(bill1)
+let tips2 = calcTip(bill2)
+let tips3 = calcTip(bill3)
+let tips = [tips1,tips2,tips3]
