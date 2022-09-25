37577b2f3d891cc6f3c11680436c342fa9a3372f
Vinicius Farias <fsvinicius9@gmail.com>
2022-09-25 16:55:09 -0300

    array concatenation

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -7,9 +7,17 @@
 // challenge yourself
 
 const temperatures = [3, -2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
-let max = temperatures[0];
 const calcTempAmplitude = function (temps) {
+  let max = temps[0];
+  let min = temps[0];
   for (let i = 0; i < temps.length; i++) {
-    
+    const curTemp = temp[i];
+    if (typeof cirTemp !== "number") continue;
+    if (curTemp > max) max = temps;
+    if (curTemp > max) min = temps;
   }
+  console.log(max, min);
+  return max - min;
 };
+
+calcTempAmplitude([3, 7, 4]);
