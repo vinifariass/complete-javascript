9e111bf43e76950d10dcf5532be0f5bfb56184c4
Vinicius Farias <fsvinicius9@gmail.com>
2022-10-01 19:29:44 -0300

    querySelector

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,2 +1,9 @@
 'use strict';
-document.querySelector('.message')
+// read the textContext property of this element
+
+document.querySelector('message').textContent = 'Correnct Number| 🎉';
+Document.querySelector('.number') = 13;
+document.querySelector('.score') = 20 
+
+document.querySelector('.guess').value=23
+document.querySelector('.guess').value
