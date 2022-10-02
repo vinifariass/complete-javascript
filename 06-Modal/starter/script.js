bbf0f74d1fefc1dae9df00c87b72a06ef93d60c7
Vinicius Farias <fsvinicius9@gmail.com>
2022-10-02 19:40:39 -0300

    Refactoring code

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,25 @@
 'use strict';
+const modal = document.querySelector(".modal")
+const overlay = document.querySelector(".overlay")
+const btnClsoeModal = document.querySelector(".close-modal")
+const btnsOpenModal = document.querySelector(".show-modal")
+
+console.log(btnsOpenModal)
+
+const openModal= function(){
+  modal.classList.remove('hidden')
+  overlay.classList.remove('hidden')
+
+}
+const closeModal= function(){
+  modal.classList.add('hidden')
+  overlay.classList.add('hidden')
+
+}
+for (let index = 0; index < btnsOpenModal.length; index++) {
+ btnsOpenModal[i].addEventListener('click', openModal);
+  btnClsoeModal.addEventListener('click', closeModal)
+
+  overlay.addEventListener('click', closeModal)
+
+}
