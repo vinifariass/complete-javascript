1eea5822158b2d49ab2e8a08767c0db752052fe4
Vinicius Farias <fsvinicius9@gmail.com>
2022-10-02 18:18:13 -0300

    score elements

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,9 +1,40 @@
 'use strict';
 // read the textContext property of this element
-
-document.querySelector('message').textContent = 'Correnct Number| 🎉';
-Document.querySelector('.number') = 13;
-document.querySelector('.score') = 20 
-
-document.querySelector('.guess').value=23
+const secretNumber = Math.trunc(Math.random() * 20) + 1
+Document.querySelector('.number').textContent = secretNumber;
+let score = 20;
+document.querySelector('.guess').value = 23
 document.querySelector('.guess').value
+//adiciona um evento 
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = document.querySelector('.guess').value;
+  if (!guess) {
+    document.querySelector('message').textContent = 'No number|';
+
+  } else if (guess == secretNumber) {
+    document.querySelector('message').textContent = 'Correnct Number| 🎉';
+
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+
+      Document.querySelector('.message').textContent = '📈Too high'
+      document.querySelector('.score').textContent = score
+      score--
+    } else {
+      Document.querySelector('.message').textContent = 'You lost the game 🧨'
+      document.querySelector('.score').textContent = 0
+    }
+  } else if (guess < secretNumber) {
+
+    if (score > 1) {
+
+      Document.querySelector('.message').textContent = '📉Too low'
+      document.querySelector('.score').textContent = score
+      score--
+    } else {
+      Document.querySelector('.message').textContent = 'You lost the game 🧨'
+      document.querySelector('.score').textContent = 0
+    }
+
+  }
+})
