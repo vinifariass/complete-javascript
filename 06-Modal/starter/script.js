12d3835b07d6dd18d0a036559d9ab427b121811d
Vinicius Farias <fsvinicius9@gmail.com>
2022-10-02 20:08:05 -0300

    Project of close modal

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -6,20 +6,29 @@ const btnsOpenModal = document.querySelector(".show-modal")
 
 console.log(btnsOpenModal)
 
-const openModal= function(){
+const openModal = function () {
   modal.classList.remove('hidden')
   overlay.classList.remove('hidden')
 
 }
-const closeModal= function(){
+const closeModal = function () {
   modal.classList.add('hidden')
   overlay.classList.add('hidden')
 
 }
 for (let index = 0; index < btnsOpenModal.length; index++) {
- btnsOpenModal[i].addEventListener('click', openModal);
+  btnsOpenModal[i].addEventListener('click', openModal);
   btnClsoeModal.addEventListener('click', closeModal)
 
   overlay.addEventListener('click', closeModal)
+  //lift our fing off the keyboard -> keyup
 
+  document.addEventListener('keydown', function (e) {
+    // any key pressed that happen ->keydown
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+
+      closeModal()
+    }
+
+  })
 }
