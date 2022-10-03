d65217e4b22e137b10eba95420eac9bbdc5d5fc3
Vinicius Farias <fsvinicius9@gmail.com>
2022-10-03 20:19:06 -0300

    alternando imagens do dado

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,21 @@
 'use strict';
+
+// Selecting elements with id
+const score0El = document.querySelector('#score--0')
+const score1El = document.getElementById('score--1')
+const diceEl = document.querySelector('.dice')
+const btnRoll = document.querySelector('.btn--roll')
+const btnNew = document.querySelector('.btn--new')
+const btnHold = document.querySelector('.btn--hold')
+score0El.textContent = 0
+score1El.textContent = 0
+diceEl.classList.add('hidden')
+
+btnRoll.addEventListener('click',function(){
+  const dice = Math.trunc(Math.random()*6)+1
+
+  //2 Display dice
+
+  diceEl.classList.remove('hidden')
+diceEl.src = `dice-${dice}.png`
+})
