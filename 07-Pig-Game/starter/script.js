243c92164f8629dee138bfd5ddca1a497d3dbbf2
Vinicius Farias <fsvinicius9@gmail.com>
2022-10-03 20:23:39 -0300

    PickGame

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -7,15 +7,26 @@ const diceEl = document.querySelector('.dice')
 const btnRoll = document.querySelector('.btn--roll')
 const btnNew = document.querySelector('.btn--new')
 const btnHold = document.querySelector('.btn--hold')
+const current0El = document.getElementById('current--0')
+const current1El = document.getElementById('current--1')
+let currentScore = 0;
 score0El.textContent = 0
 score1El.textContent = 0
 diceEl.classList.add('hidden')
 
-btnRoll.addEventListener('click',function(){
-  const dice = Math.trunc(Math.random()*6)+1
+btnRoll.addEventListener('click', function () {
+  const dice = Math.trunc(Math.random() * 6) + 1
 
   //2 Display dice
 
   diceEl.classList.remove('hidden')
-diceEl.src = `dice-${dice}.png`
+  diceEl.src = `dice-${dice}.png`
+  if (dice !== 1) {
+    //Add dice to current score
+    currentScore += dice;
+    current0El.textContent = currentScore; //CHANGE LATER
+  } else {
+    //Switch to next player
+
+  }
 })
