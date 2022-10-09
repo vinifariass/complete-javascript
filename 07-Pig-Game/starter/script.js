ba444355b715141628cffa22e22f7d87eaceb26e
Vinicius Farias <fsvinicius9@gmail.com>
2022-10-09 09:29:47 -0300

    switching the active player

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 // Selecting elements with id
+const player0El = document.querySelector('.player--0')
+const player1El = document.querySelector('.player--1')
 const score0El = document.querySelector('#score--0')
 const score1El = document.getElementById('score--1')
 const diceEl = document.querySelector('.dice')
@@ -11,6 +13,8 @@ const current0El = document.getElementById('current--0')
 const current1El = document.getElementById('current--1')
 let currentScore = 0;
 score0El.textContent = 0
+let activePlayer = 0;
+const scores = [0, 0]
 score1El.textContent = 0
 diceEl.classList.add('hidden')
 
@@ -24,9 +28,13 @@ btnRoll.addEventListener('click', function () {
   if (dice !== 1) {
     //Add dice to current score
     currentScore += dice;
+    document.getElementById(`current--${activePlayer}`).textContent = currentScore
     current0El.textContent = currentScore; //CHANGE LATER
   } else {
     //Switch to next player
-
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    currentScore = 0;
+    player0El.classList.toggle('player--active')
+    player1El.classList.toggle('player--active')
   }
 })
