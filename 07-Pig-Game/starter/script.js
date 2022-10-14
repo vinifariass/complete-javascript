cd807675b8e72944fbec26545233179da0a546f5
Vinicius Farias <fsvinicius9@gmail.com>
2022-10-14 16:47:04 -0300

    Hoisted declarations

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -11,30 +11,76 @@ const btnNew = document.querySelector('.btn--new')
 const btnHold = document.querySelector('.btn--hold')
 const current0El = document.getElementById('current--0')
 const current1El = document.getElementById('current--1')
-let currentScore = 0;
-score0El.textContent = 0
-let activePlayer = 0;
-const scores = [0, 0]
-score1El.textContent = 0
+
+let scores,currentScore,playing,activePlayer;
+const init = function () {
+ activePlayer = 0;
+ scores = [0, 0]
+
 diceEl.classList.add('hidden')
+   playing = true;
+  currentScore = 0;
+  score0El.textContent = 0
+  score1El.textContent = 0
+  current0El.textContent = 0
+  current1El.textContent = 0
+  diceEl.classList.add('hidden')
+  player0El.classList.remove('player--winner')
+  player1El.classList.remove('player--winner')
+  player0El.classList.add('player--active')
+  player1El.classList.remove('player--active')
+}
+
+init();
+
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = currentScore
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  currentScore = 0;
+  player0El.classList.toggle('player--active')
+  player1El.classList.toggle('player--active')
+}
+
 
 btnRoll.addEventListener('click', function () {
-  const dice = Math.trunc(Math.random() * 6) + 1
+  if (playing) {
+    const dice = Math.trunc(Math.random() * 6) + 1
 
-  //2 Display dice
+    //2 Display dice
 
-  diceEl.classList.remove('hidden')
-  diceEl.src = `dice-${dice}.png`
-  if (dice !== 1) {
-    //Add dice to current score
-    currentScore += dice;
-    document.getElementById(`current--${activePlayer}`).textContent = currentScore
-    current0El.textContent = currentScore; //CHANGE LATER
-  } else {
-    //Switch to next player
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    currentScore = 0;
-    player0El.classList.toggle('player--active')
-    player1El.classList.toggle('player--active')
+    diceEl.classList.remove('hidden')
+    diceEl.src = `dice-${dice}.png`
+    if (dice !== 1) {
+      //Add dice to current score
+      currentScore += dice;
+      current0El.textContent = currentScore; //CHANGE LATER
+    } else {
+      //Switch to next player
+      switchPlayer()
+
+    }
   }
 })
+
+btnHold.addEventListener('click', function () {
+  if (playing) {
+    scores[activePlayer] += currentScore;
+    //scores[1]= scores[1] +currentScore
+
+    document.getElementById(`current--${activePlayer}`).textContent = scores[activePlayer]
+    if (scores[activePlayer] >= 20) {
+      //Finish the game
+      playing = false;
+      document.classList.add('hidden')
+      document.querySelector(`.player--${activePlayer}`).classList.add('player--winner')
+      document.querySelector(`.player--${activePlayer}`).classList.remove('player--active')
+    } else {
+
+      switchPlayer()
+    }
+  }
+
+  // 2.Check if player's score is higher
+})
+
+btnNew.addEventListener('click',init)
--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,26 @@
 'use strict';
+
+function calcAge(birthYear) {
+  const age = 2037 - birthYear
+  function printAge() {
+    const output = `You are ${age},born in ${birthYear}`
+    console.log(output);
+  }
+  if (birthYear >= 1981 && birthYear <= 1996) {
+    var millenial = true
+    const str = `Oh, and you're a millenial ${firstName}`
+    console.log(str)
+
+    function add(a,b){
+      return a +b;
+      
+    }
+  }
+  console.log(millenial);
+  printAge()
+  return age
+}
+
+
+const firstName = 'Jonas'
+calcAge(1991)
